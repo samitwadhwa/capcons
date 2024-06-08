a92a214bd0dbc3c8941ee323acfedd2fc4ab250d
samitwadhwa <samitwadhwa@gmail.com>
2024-06-08 12:33:28 +0530

    order-details updated

--- src/app/dashboard/commerce/orders/orderdetails/page.tsx
+++ src/app/dashboard/commerce/orders/orderdetails/page.tsx
@@ -15,24 +15,60 @@ const OrderDetail: React.FC = () => {
                 <div className="flex-1 flex flex-col gap-8">
                     <div className="bg-[#2A2A2E] rounded-lg flex flex-col gap-4 h-fit">
                         <h5 className="text-h5 h5 font-semibold text-white bg-primary p-2 rounded">Payment Mode</h5>
-                        <div className='p-4'>
-                            <p>Payment Type: Credit card</p>
-                            <p>Payment Method: Razorpay</p>
-                            <p>Payment ID: 455RT</p>
-                            <p>Total Price: 6.99</p>
-                            <p>Discount: 120</p>
-                            <p>Promo: CH034</p>
-                            <p>Shipping Charges: 150.99</p>
-                            <p>Total Amount: 450.99</p>
-                            <p>Sub Total Amount: 450.99</p>
+                        <div className='p-4 space-y-2'>
+                            <div className="flex justify-between">
+                                <p>Payment Type:</p>
+                                <p>Credit card</p>
+                            </div>
+                            <div className="flex justify-between">
+                                <p>Payment Method:</p>
+                                <p>Razorpay</p>
+                            </div>
+                            <div className="flex justify-between">
+                                <p>Payment ID:</p>
+                                <p>455RT</p>
+                            </div>
+                            <div className="flex justify-between">
+                                <p>Total Price:</p>
+                                <p>6.99</p>
+                            </div>
+                            <div className="flex justify-between">
+                                <p>Discount:</p>
+                                <p>120</p>
+                            </div>
+                            <div className="flex justify-between">
+                                <p>Promo:</p>
+                                <p>CH034</p>
+                            </div>
+                            <div className="flex justify-between">
+                                <p>Shipping Charges:</p>
+                                <p>150.99</p>
+                            </div>
+                            <div className="flex justify-between">
+                                <p>Total Amount:</p>
+                                <p>450.99</p>
+                            </div>
+                            <div className="flex justify-between">
+                                <p>Sub Total Amount:</p>
+                                <p>450.99</p>
+                            </div>
                         </div>
                     </div>
                     <div className="bg-[#2A2A2E] rounded-lg flex flex-col gap-4 h-fit">
                         <h5 className="text-h5 h5 font-semibold text-white bg-primary p-2 rounded">Customer Details</h5>
-                        <div className='p-4'>
-                            <p>Name: Ramesh</p>
-                            <p>Phone Number: 954493434</p>
-                            <p>Address: D-197, Street Number 8, Ramesh Park, Laxmi Nagar, Delhi, 110092</p>
+                        <div className='p-4 space-y-2'>
+                            <div className="flex justify-between">
+                                <p>Name:</p>
+                                <p>Ramesh</p>
+                            </div>
+                            <div className="flex justify-between">
+                                <p>Phone Number:</p>
+                                <p>954493434</p>
+                            </div>
+                            <div className="flex justify-between">
+                                <p>Address:</p>
+                                <p>Laxmi Nagar, Delhi, 110092</p>
+                            </div>
                         </div>
                     </div>
                 </div>
